13573a6ba8e38357de6922e4019273cf4b40ac33
michal92299 <chudybyczek2137@gmail.com>
2026-05-14 17:54:29 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
+# LegendaryOS CLI Tool - main.rb
+# Entry point for the legendary command-line interface
+
+$LOAD_PATH.unshift(__dir__)
+$LOAD_PATH.unshift(File.join(__dir__, "src"))
+
+require "cli"
+
+LegendaryOS::CLI.new(ARGV).run
